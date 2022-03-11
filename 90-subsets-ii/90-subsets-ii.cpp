48f7c5945cb3b4a442fcbd7f3a6367be71e48538
Himanshu Singh <72404447+Himanshuxn@users.noreply.github.com>
2022-03-11 18:46:16 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (89.28%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+    void bt(int ind,vector<int>& nums,vector<vector<int>> &ans, vector<int>& li ) {
+    ans.push_back(li);
+    for(int i=ind;i<nums.size();i++)
+    {
+        if(i!=ind && nums[i]==nums[i-1]) continue;
+        li.push_back(nums[i]);
+        bt(i+1,nums,ans,li);
+        li.pop_back();
+    }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+     vector<int> li;
+     vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        bt(0,nums,ans,li);
+        return ans;
+    }
+};
