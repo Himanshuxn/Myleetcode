55f4c3363ec907b3ffb758a0dec129e5d409d118
Himanshu Singh <72404447+Himanshuxn@users.noreply.github.com>
2022-05-13 23:51:39 +0530

    Time: 13 ms (40.22%), Space: 13.9 MB (99.86%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+	int finalValueAfterOperations(vector<string>& operations) {
+		int res = 0;
+		for(int i = 0; i < operations.size(); i++){
+			(operations[i][1] == '+') ? res++ : res--;
+		}
+		return res;
+	}
+};
