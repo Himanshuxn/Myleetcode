968721dbc8dc15866d38bb1b55de4cc6b2480a04
Himanshu Singh <72404447+Himanshuxn@users.noreply.github.com>
2022-06-02 18:40:22 +0530

    Time: 0 ms (100.00%), Space: 7 MB (31.45%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,48 +1,56 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> v;
-     int left  = 0;
+     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+     int m = matrix.size();
+     int n = matrix[0].size();
+     vector<int> v;
      int top = 0;
-     int right = matrix[0].size()-1;
-     int bottom = matrix.size()-1;
-        int direction = 1;
-    while(left<=right && top <=bottom)
-    {
-        if(direction == 1){
-        for( int i = left;i<=right;i++)
-        {
-            v.push_back(matrix[top][i]);
+     int left = 0;
+     int right = n-1;
+     int buttom = m-1;
+    int dir = 1;
+     while(top<=buttom && right>=left)
+     {
+        if(dir==1)
+        {for(int i =left ;i<=right;i++)
+         {
+          v.push_back(matrix[top][i]);
+         
+         }
+          top++;
+         dir=2;
         }
-        top++;
-        direction =2;
-        }
-         else if(direction == 2){
-        for( int i=top;i<=bottom;i++)
-        {
+        else if(dir==2)
+         {
+         for(int i =top ;i<=buttom;i++)
+         {
           v.push_back(matrix[i][right]);
-        }
-        right--;
-        direction =3;
+          
          }
-         else if(direction == 3){
-        for(int i = right ;i>=left;i--)
-        {
-          v.push_back(matrix[bottom][i]);  
-        }
-        bottom --;
-            direction =4;
-             
+         right--;
+             dir=3;
          }
-         else if(direction == 4){
-        for(int i = bottom ; i>=top;i--)
-        {
-         v.push_back(matrix[i][left]);   
-        }
-        left++;
-             direction = 1;
+        else if(dir==3)
+         {
+         for(int i =right ;i>=left;i--)
+         {
+          v.push_back(matrix[buttom][i]);
+          
          }
-    }
-    return v;
+         buttom--;
+             dir=4;
+         }
+        else if(dir==4)
+         {
+         for(int i =buttom ;i>=top;i--)
+         {
+          v.push_back(matrix[i][left]);
+          
+         }
+         left++;
+        dir=1;
+         }
+     }
+        return v;
     }
 };
